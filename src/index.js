e9a19614dd97ac6a6f5e2cf1623f194ad2f1113d
Squizyyyyy <squizy.boy@mail.ru>
2025-08-08 02:10:22 +0300

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'leaflet/dist/leaflet.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+
+// Регистрируем service worker для PWA
+serviceWorkerRegistration.register();
