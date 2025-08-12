60144802145650f914d93f7c78f43094bb451f05
Squizyyyyy <squizy.boy@mail.ru>
2025-08-12 22:56:21 +0300

    update

--- src/MapView.js
+++ src/MapView.js
@@ -149,9 +149,12 @@ export default function MapView() {
         style={{ height: '100%' }}
       >
         <TileLayer
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-          attribution='&copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
+          url="https://cartodb-basemaps-a.global.ssl.fastly.net/light_all/{z}/{x}/{y}{r}.png"
+          subdomains="abcd"
+          detectRetina={true}
         />
+
         <LocationMarker onAddMarker={onAddMarker} />
         {markers.map((marker) => (
           <Marker
