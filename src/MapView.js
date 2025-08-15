a83dc875c2b817311b2d46f7f460d871569bb609
Squizyyyyy <squizy.boy@mail.ru>
2025-08-15 23:16:34 +0300

    Update MapView.js

--- src/MapView.js
+++ src/MapView.js
@@ -150,7 +150,7 @@ export default function MapView() {
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}{r}.png"
+          url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}.png"
 		  subdomains={['a','b','c']}
 		  detectRetina={true}
         />
