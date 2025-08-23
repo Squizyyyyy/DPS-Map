08b543f288d7e9596545b5acb219c31bc1c17525
Squizyyyyy <squizy.boy@mail.ru>
2025-08-24 02:52:42 +0300

    Update MainPage.js

--- src/MainPage.js
+++ src/MainPage.js
@@ -298,8 +298,8 @@ export default function MainPage() {
               onClick={() => setActiveTab("account")}
               style={{
                 position: "absolute",
-                top: 11.5,
-                right: 12,
+                top: 10,
+                right: 11,
                 width: 40,
                 height: 40,
                 borderRadius: "50%",
