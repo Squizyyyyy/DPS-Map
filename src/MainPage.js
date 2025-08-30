1311aa198b8193ae91b1018b7ed742135b93a68e
Squizyyyyy <squizy.boy@mail.ru>
2025-08-30 17:22:26 +0300

    Update MainPage.js

--- src/MainPage.js
+++ src/MainPage.js
@@ -301,7 +301,7 @@ export default function MainPage() {
           disabled={!sdkReady || loadingLogin}
           style={{
             marginTop: 16,
-            padding: "14px 32px",
+            padding: "12px 32px",
             background: sdkReady
               ? `linear-gradient(90deg, #2787f5, #0a90ff)`
               : "#6c757d",
