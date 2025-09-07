3eacd69b1c1c5aaa0c6212143ea556ee85728260
Squizyyyyy <squizy.boy@mail.ru>
2025-09-07 14:03:07 +0300

    Update MainPage.js

--- src/MainPage.js
+++ src/MainPage.js
@@ -282,7 +282,7 @@ export default function MainPage() {
       script.setAttribute("data-userpic", "false");
       script.setAttribute("data-radius", "8");
       script.setAttribute("data-request-access", "write");
-      script.setAttribute("data-onauth", "handleTelegramAuth(user)");
+      script.setAttribute("data-onauth", "handleTelegramAuth");
 	  script.async = true;
 	  
 	  container.appendChild(script);
